a442f6babc62b648bf52050904656cd088ded000
Andrey Popp <8mayday@gmail.com>
2015-01-20 15:34:40 +0800

    example: comments

--- example/index.ts
+++ example/index.ts
@@ -4,6 +4,11 @@ require('./index.css');
 
 import React = require('react/addons');
 import Button = require('./Button');
+/**
+ * If we don't use AsyncComponent at the value level, tsc won't emit require
+ * statement and so we won't have it bundled. Though type info is acquired and
+ * we can load the module later and assign the type to it.
+ */
 import AsyncComponent = require('./AsyncComponent');
 
 var App = React.createClass({
